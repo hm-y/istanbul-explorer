f71cf7a416b4677a4f7c8c5a7b39bdec54838129
Hilmi Murat YILDIRIM <this.is.yildirim@gmail.com>
2018-01-19 16:48:30 -0500

    feat: Implement foursquare ajax request

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 // ----- Data ----- //
-
 // Category indexes: 0 - Historical 1- Museum 2- Shopping  3- Others
 var places = ko.observableArray([
-    {id:0, name: "Gülhane Park", location: {lat: 41.015268, lng: 28.981133}, category: 3},
-    {id:1, name: "The Blue Mosque", location: {lat: 41.006203, lng: 28.976771}, category: 0},
-    {id:2, name: "Dolmabahçe Palace", location: {lat: 41.040378, lng: 29.000459}, category: 0},
-    {id:3, name: "Grand Bazaar", location: {lat: 41.011672, lng: 28.968154}, category: 2},
-    {id:4, name: "Basilica Cistern", location: {lat: 41.009404, lng: 28.977792}, category: 0},
-    {id:5, name: "Museum of Modern Art", location: {lat: 41.035135, lng: 28.983823}, category: 1},
-    {id:6, name: "Topkapi Palace Museum", location: {lat: 41.012410, lng: 28.983422}, category: 1},
-    {id:7, name: "Maiden's Tower", location: {lat: 41.022044, lng: 29.004239}, category: 3},
-    {id:8, name: "Miniatürk", location: {lat: 41.067660, lng: 28.948880}, category: 1}
+    {id:0, name: "Gülhane Park", location: {lat: 41.015268, lng: 28.981133},
+        venue: "4bf13f1921072d7f4cb9208b" , category: 3},
+    {id:1, name: "Rumeli Hisar Museum", location: {lat: 41.085728, lng: 29.056912},
+        venue: "4eb5352b9a52ad1e3ed3ec34" ,  category: 0},
+    {id:2, name: "Dolmabahçe Palace", location: {lat: 41.040378, lng: 29.000459},
+        venue: "4c3220f8ac0ab713a5671c1e" ,  category: 0},
+    {id:3, name: "Grand Bazaar", location: {lat: 41.011672, lng: 28.968154},
+        venue: "4c09fd76009a0f476ac2e8bf" ,  category: 2},
+    {id:4, name: "Basilica Cistern", location: {lat: 41.009404, lng: 28.977792},
+        venue: "4b72a221f964a52055812de3" ,  category: 0},
+    {id:5, name: "Museum of Modern Art", location: {lat: 41.035135, lng: 28.983823},
+        venue: "4cda9d2b6ad1a093eb1ae756" ,  category: 1},
+    {id:6, name: "Topkapi Palace Museum", location: {lat: 41.012410, lng: 28.983422},
+        venue: "4b824a4bf964a5202dcf30e3" ,  category: 1},
+    {id:7, name: "Maiden's Tower", location: {lat: 41.022044, lng: 29.004239},
+        venue: "4bc599f96a3e9c74d407f648" ,  category: 3},
+    {id:8, name: "Büyük Çamlıca Tepesi", location: {lat: 41.030155, lng: 29.068368},
+        venue: "4ce0dd7adb1254818cf33ece" ,  category: 3}
 ]);
-
-var categories = ["Historical", "Museum", "Shopping", "Others"];
-
-
+var photo_src = ko.observable("");
 // ----- Controller ----- //
 var filter = ko.observable(-1);
 var listing = ko.computed(function(){
@@ -44,6 +49,7 @@ $('button').click(function() {
 });
 
 ko.applyBindings(listing());
+
 
 // ----- View ----- //
 
@@ -255,7 +261,7 @@ var styles = [
 var initMap = function() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 41.025604, lng: 28.997002},
-        zoom: 13,
+        zoom: 12,
         styles: styles,
         disableDefaultUI: true
     });
@@ -283,25 +289,46 @@ var addMarkerList = function(place){
     var markerData = {
         map: map,
         icon: icon,
+        venue: place.venue,
         position: place.location,
         name: place.name
     };
     var marker = new google.maps.Marker(markerData);
 
     marker.addListener("click", function() {
+
         openWindow(marker, markerData);
     });
 
     markers.push(marker);
 };
 
+// ----- InfoWindow with Ajax: FourSquare ----- //
 var openWindow = function(marker, markerData){
+    var address = "https://api.foursquare.com/v2/venues/" + markerData.venue
+        + "/photos?&client_id=KW4TUJ5L2ZFWAZXD20ZFOEJKD5RTLNXVGALY0S03HVO13LZI"
+        + "&client_secret=HA10R3RBCU4ODXIXNUWMAC45EOON4MYGR0C5XDJMKZHX3AIF&v=20180119";
+
     if(infoWin)
         prevWin = infoWin;
-    infoWin = new google.maps.InfoWindow({
-        content: markerData.name
-    });
     if (prevWin)
         prevWin.close();
-    infoWin.open(map, marker);
+
+    $.ajax({
+        url: address,
+        dataType: 'json',
+        success: function(data){
+            var photo = data.response.photos.items[0];
+            photo_src(photo.prefix + "200x200" + photo.suffix);
+        }
+    }).done(function(){
+        var html = "<h3>" + markerData.name + "</h3>"
+            + "<img src=\"" + photo_src() + "\">";
+
+        infoWin = new google.maps.InfoWindow({
+            content: html
+        });
+        infoWin.open(map, marker);
+    });
+
 };
